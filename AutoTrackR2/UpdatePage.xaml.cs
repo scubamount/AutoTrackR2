5006db15eaa70fcf9b52fc0a9cc1214daddf336b
BubbaGumpShrump <joshua.p.farrell@gmail.com>
2024-12-20 01:11:34 -0500

    x

--- AutoTrackR2/UpdatePage.xaml.cs
+++ AutoTrackR2/UpdatePage.xaml.cs
@@ -9,7 +9,7 @@ namespace AutoTrackR2
 {
     public partial class UpdatePage : UserControl
     {
-        private string currentVersion = "v2.03-release";
+        private string currentVersion = "v2.04-release";
         private string latestVersion;
 
         public UpdatePage()
